d1dbf5b3d622ac27162e22896f101d81558ed9f7
JingSen Liu <jsliu521@gmail.com>
2012-08-21 09:44:53 +0800

    remove production site config

--- features/support/config.rb
+++ features/support/config.rb
@@ -1,6 +1,5 @@
 WEB13_URL = 'http://192.168.0.13/login.asp'
 WEB173_URL = 'http://192.168.0.173/login.asp'
-WEBTHC_URL = 'https://www.thcdecisions.com'
 
 DB13HOST = '192.168.0.177'
 DB13USER = 'sa'
@@ -13,4 +12,3 @@ PRICING_DATE = '1/10/2012'
 
 $result_13 = {}
 $result_173 = {}
-$result_thc = {}
